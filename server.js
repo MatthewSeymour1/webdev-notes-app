9dacc7f05ba7b1643e637bb7ac7081d57b18b3cb
Matthew Seymour (Student) <N00233944@iadt.ie>
2026-09-14 16:51:40 +0100

    npm start     to run it

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
-
 const notes = [
   { id: 1, text: "Buy milk" },
   { id: 2, text: "Finish the deployment tutorial" },
@@ -21,6 +17,6 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.listen(8000, () => {
-  console.log("Listening on http://localhost:8000");
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
 });
