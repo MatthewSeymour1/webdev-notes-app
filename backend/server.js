771ea6992ca6bd424770a10c8a07c5241b12a8d8
Matthew Seymour (Student) <N00233944@iadt.ie>
2026-09-16 10:42:26 +0100

    added another note to test render

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const notes = [
   { id: 1, text: "Buy one hundred milk" },
   { id: 2, text: "Finish the deployment tutorial" },
   { id: 3, text: "Water the plants" },
+  { id: 4, text: "Watering ALL the plants" },
 ];
 
 app.use(express.static("public"));
